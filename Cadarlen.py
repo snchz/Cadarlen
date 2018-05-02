fc9b0bc2d452aa84bdd80d6033e31ad220dd67ea
snchz <ssaanncchheezz@gmail.com>
2018-05-02 14:10:48 +0200

    Solucionado problemas funcionales
    Como el insert en Calendar. El idEvento no se generaba bien.

--- Cadarlen.py
+++ Cadarlen.py
@@ -14,46 +14,82 @@ class colores:
 	BOLD = '\033[1m'
 	UNDERLINE = '\033[4m'
 
+
+class Pantalla:	
+	##################################
+	#Mientras mostramos lo siguiente podemos hacer calculos
+	##################################
+	def cargando(self):
+		a=1
+		salto=1
+		while a<50:
+			self.limpiar()
+			inc=randint(0,salto)
+			salto=salto*2
+			a=a+inc
+			if a>50:
+				a=50
+			print("["+"".rjust(a,'*')+"".rjust(50-a,' ')+"] "+str(a*2)+"%")
+			time.sleep(0.5)
+	def limpiar(self):
+		#Limpiar pantalla
+		if os.name=='posix':
+			os.system('clear')
+		elif os.name =='nt':
+			os.system('cls')
+
+	
 class BD:
-	#
 	def __init__(self, nf):
-		dummy=1
-		#TODO
+		self.nf=nf
+	#Retorna el lastrowid
+	def insert(self, query, arrayParametros):
+		print("Dentroooo")
+		db = sqlite3.connect(self.nf)
+		cu = db.cursor()
+		cu.execute(query,arrayParametros)
+		lastId=cu.lastrowid
+		print("last "+str(lastId))
+		db.commit()
+		cu.close()
+		db.close()
+		return lastId
+	def select(self, query, arrayParametros):
+		db=sqlite3.connect(self.nf)
+		cu=db.cursor()
+		cu.execute(query, arrayParametros)
+		filas=cu.fetchall()
+		cu.close() #Eliminar si no funciona el select
+		db.close() #Eliminar si no funciona el select
+		return filas
 		
-class Evento:
+		
+		
+class Evento(BD):
 	#Se pasan los valores como string, internamente hacemos el tipado de datos
 	def __init__(self,idUsuario, fechaHoraInicio, fechaHoraFin, idPeriocidad, fechaFin, descripcion):
+		BD.__init__(self,"datos.db")
 		self.idUsuario = int(idUsuario)
 		self.fechaHoraInicio = datetime.datetime.strptime(fechaHoraInicio, "%Y-%m-%d %H:%M")
 		self.fechaHoraFin = datetime.datetime.strptime(fechaHoraFin, "%Y-%m-%d %H:%M")
 		self.idPeriocidad = int(idPeriocidad)
 		self.fechaFin = datetime.datetime.strptime(fechaFin, "%Y-%m-%d")
 		self.descripcion=descripcion
-		self.insert()
-	#inserto en base de datos el Evento.
-	def insert(self):
-		nf = "datos.db"
-		db = sqlite3.connect(nf)
-		cu = db.cursor()
-		cu.execute("INSERT INTO Eventos (idUsuario,fechaHoraInicio,fechaHoraFin,idPeriocidad,fechaFin,descripcion) VALUES(?,?,?,?,?,?)", [self.idUsuario,self.fechaHoraInicio,self.fechaHoraFin,self.idPeriocidad,self.fechaFin,self.descripcion])
-		self.idEvento=cu.lastrowid
-		db.commit()
-		cu.close()
-		db.close()
+		#inserto en base de datos el Evento.
+		#print("Insertando en la la tabla Eventos")
+		self.idEvento=self.insert("INSERT INTO Eventos (idUsuario,fechaHoraInicio,fechaHoraFin,idPeriocidad,fechaFin,descripcion) VALUES(?,?,?,?,?,?)", [self.idUsuario,self.fechaHoraInicio,self.fechaHoraFin,self.idPeriocidad,self.fechaFin,self.descripcion])
+		#print("Evento "+str(self.idEvento)+" insertado")
 	#obtengo la configuracion de Periocidad (tipo,incremento) Ej: Semanal-> ('D',7)
 	def obtenerPeriocidad(self):
-		nf="datos.db"
-		db=sqlite3.connect(nf)
-		cu=db.cursor()
-		cu.execute("SELECT tipo,incremento FROM Periocidades WHERE idPeriocidad=?",[self.idPeriocidad])
-		filas=cu.fetchall()
+		filas=self.select("SELECT tipo,incremento FROM Periocidades WHERE idPeriocidad=?",[self.idPeriocidad])
 		for fila in filas:
 			return (fila[0],fila[1])
 		return (None,None)
 
-class Calendario:
+class Calendario(BD):
 	#usuario y pass sin codificar
 	def __init__(self, usuario, password, crear):
+		BD.__init__(self,"datos.db")
 		if crear==1:
 			self.insertarUsuario(usuario,password)
 		elif crear==0:
@@ -64,98 +100,54 @@ class Calendario:
 			raise Exception("No se ha logrado login.")
 	def insertarUsuario(self,usuario,password):
 		hashPass=hashlib.sha224(password.encode('utf-8')).hexdigest()
-		nf="datos.db"
-		db=sqlite3.connect(nf)
-		cu=db.cursor()
-		cu.execute("INSERT INTO Usuarios (nombre, password) VALUES(?,?)",[usuario,hashPass])
-		db.commit()
-		cu.close()
-		db.close()
+		self.insert("INSERT INTO Usuarios (nombre, password) VALUES(?,?)",[usuario,hashPass])
 		
 	#Login, busca el usuario y password y si existe retorna el id de usuario
 	#Si el login es incorrecto retorna -1
 	def login(self,usuario,password):
 		hashPass=hashlib.sha224(password.encode('utf-8')).hexdigest()
-		
-		nf="datos.db"
-		db=sqlite3.connect(nf)
-		cu=db.cursor()
-		cu.execute("SELECT idUsuario FROM Usuarios WHERE nombre=? AND password=?",[usuario,hashPass])
-		filas=cu.fetchall()
+		filas=self.select("SELECT idUsuario FROM Usuarios WHERE nombre=? AND password=?",[usuario,hashPass])
 		for fila in filas:
 			return int(fila[0])
 		return -1
 	#Pinta por salida de pantalla la vista VCalendario
 	def pintar(self):
 		#TODO
-		nf="datos.db"
-		db=sqlite3.connect(nf)
-		cu=db.cursor()
-		cu.execute("SELECT * FROM VCalendario WHERE idUsuario=?",[self.idUsuario])
-		filas=cu.fetchall()
+		filas=self.select("SELECT * FROM VCalendario WHERE idUsuario=?",[self.idUsuario])
 		print(colores.OKBLUE+"### CALENDARIO ####"+colores.ENDC)
 		for fila in filas:
 			print(fila)
 		print(colores.OKBLUE+"### ### ### ### ###"+colores.ENDC)
-		cu.close()
-		db.close()
 	#Pinta las diferentes Periocidades que existen en tabla
 	def pintarPeriocidades(self):
 		#TODO
-		nf="datos.db"
-		db=sqlite3.connect(nf)
-		cu=db.cursor()
-		cu.execute("SELECT idPeriocidad, descripcion FROM Periocidades ORDER BY idPeriocidad ASC")
-		filas=cu.fetchall()
+		filas=self.select("SELECT idPeriocidad, descripcion FROM Periocidades ORDER BY idPeriocidad ASC",[])
 		print("Periocidades:")
 		for fila in filas:
 			print("\t{0}. {1}".format(fila[0],fila[1]))
-		cu.close()
-		db.close()
 	#Elimina el evento de todo el calendario
 	def eliminarEvento(self,idEvento):
 		#TODO
-		nf="datos.db"
-		db=sqlite3.connect(nf)
-		cu=db.cursor()
-		cu.execute("DELETE FROM Eventos WHERE idEvento=?",[idEvento])
-		cu.execute("DELETE FROM Calendario WHERE idEvento=?",[idEvento])
-		db.commit()
-		cu.close()
-		db.close()
+		self.insert("DELETE FROM Eventos WHERE idEvento=?",[idEvento])
+		self.insert("DELETE FROM Calendario WHERE idEvento=?",[idEvento])
 	#Elimina del calendario el evento int, pero solo para una fecha tipada determinada
 	def eliminarEventoFecha(self,idEvento,fecha):
 		#TODO
-		nf="datos.db"
-		db=sqlite3.connect(nf)
-		cu=db.cursor()
-		cu.execute("DELETE FROM Calendario WHERE idEvento=? AND fechaHoraInicio>=? AND fechaHoraInicio<date(?,'+1 day')",[idEvento,fecha,fecha])
-		db.commit()
-		cu.close()
-		db.close()
+		self.insert("DELETE FROM Calendario WHERE idEvento=? AND fechaHoraInicio>=? AND fechaHoraInicio<date(?,'+1 day')",[idEvento,fecha,fecha])
 	#Dado un evento, agrega al calendario en loop dependiendo de la periocidad del evento
 	def agregar(self,evento):
 		(tipo,incremento) = evento.obtenerPeriocidad()
-		print("tipo: "+str(tipo)+" incremento: "+str(incremento))
-		nf="datos.db"
-		db=sqlite3.connect(nf)
-		cu=db.cursor()
 		fechaHoraI=evento.fechaHoraInicio
 		fechaHoraF=evento.fechaHoraFin
-		fechaFin=evento.fechaFin
-		while fechaHoraI<=fechaFin:
-			cu.execute("INSERT INTO Calendario VALUES(?,?,?)",[fechaHoraI,fechaHoraF,evento.idEvento])
-			db.commit()
+		fechaF=evento.fechaFin
+		while (fechaHoraI<=fechaF):
+			self.insert("INSERT INTO Calendario (fechaHoraInicio, fechaHoraFin, idEvento) VALUES(?,?,?)",[fechaHoraI,fechaHoraF,evento.idEvento])
 			if tipo=='D':
 				fechaHoraI=fechaHoraI+timedelta(days=incremento)
 				fechaHoraF=fechaHoraF+timedelta(days=incremento)
 			elif tipo=='M':
 				fechaHoraI=fechaHoraI+relativedelta(months=incremento)
 				fechaHoraF=fechaHoraF+relativedelta(months=incremento)
-				
-		cu.close()
-		db.close()
-
 c=None
 
 ##################################
@@ -163,9 +155,13 @@ c=None
 ##################################
 logado=0
 while logado==0:
-	print("".rjust(100,"#"))
-	login=int(input("[1] Login o [2] Crear Usuario: "))
-	if login==2:
+	print(colores.HEADER+"".rjust(100,"#")+colores.ENDC)
+	login=0
+	try:
+		login=int(input("[1] Login o [2] Crear Usuario: "))
+	except:
+		login=0
+	if login==2: #Crear usuario
 		usuario=input("Nuevo usuario: ")
 		password=input("Password: ")
 		password2=input("Repetir password: ")
@@ -175,7 +171,7 @@ while logado==0:
 			c=Calendario(usuario, password,1)
 			print("Usuario Creado.")
 			time.sleep(2)
-	else:
+	elif login==1:
 		usuario=input("Usuario: ")
 		password=input("Password: ")
 		try:
@@ -183,29 +179,14 @@ while logado==0:
 			logado=1
 			print("Login correcto. Hola "+usuario)
 		except:
+			Pantalla().limpiar()
 			print("Usuario o password incorrecto.")
+	else:
+		Pantalla().limpiar()
+		print("OpciÃ³n incorrecta.")
 time.sleep(2)
 
-
-
-##################################
-#Mientras mostramos lo siguiente podemos hacer calculos
-##################################
-a=1
-salto=1
-while a<50:
-	#Limpiar pantalla
-	if os.name=='posix':
-		os.system('clear')
-	elif os.name =='nt':
-		os.system('cls')
-	inc=randint(0,salto)
-	salto=salto*2
-	a=a+inc
-	if a>50:
-		a=50
-	print("["+"".rjust(a,'*')+"".rjust(50-a,' ')+"] "+str(a*2)+"%")
-	time.sleep(0.5)
+Pantalla().cargando()
 
 ##################################
 #Si logado entonces opciones
@@ -216,7 +197,11 @@ while logado!=0:
 	print("1. Agregar")
 	print("2. Eliminar evento")
 	print("3. Eliminar evento de una fecha")
-	opcion=int(input("Seleccione opcion: "))
+	opcion=0
+	try:
+		opcion=int(input("Seleccione opcion: "))
+	except:
+		opcion=0
 	if opcion==1:
 		fhi=None
 		try:
